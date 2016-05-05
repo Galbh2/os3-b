7fec971b4e9914247c8649df83db476ee1fd6b9b
gbenhaim <galbh2@gmail.com>
2016-05-05 03:58:41 +0300

    Implemented Copier.c I have put some comments in the code about the changes I made, and also some more tasks we have. We also need to go over the code and make it cleaner.

--- Copier.c
+++ Copier.c
@@ -45,9 +45,16 @@ void *run_listener(void *param) {
 	int fd, num, len, start, status;
 	char filename[READ_BUFF_SIZE];
 	char *fn_ptr;
+	char* path;
+
+	//TODO check what are the unused variables
 
 	// Get the pointer to the parameters struct
 	data = (ListenerData*)param;
+
+	// Get the pointer to the BoundedBuffer
+
+	buff = data->buff;
 
 	// Create the pipe
 	mknod(data->pipe, S_IFIFO | FILE_ACCESS_RW, 0);
@@ -61,8 +68,30 @@ void *run_listener(void *param) {
 			// Parse read data into file names, enqueue them, etc...
 			// TODO: Complete this part
 			// TODO: Make sure the thread exits at some point
+
+			char* token;
+			token = strtok(filename, "\n");
+
+			while (token != NULL) {
+
+				//memory allocation for the new path
+				path = (char*) malloc(sizeof(char) * strlen(token));
+				strcpy(path, token);
+				token = strtok(NULL, "\n");
+				printf("%s", token);
+
+				// checks if the program wants to exit
+				// free memory, close files and remove the pipe
+				if (!bounded_buffer_enqueue(buff, path)) {
+					free(path);
+					close(fd);
+					remove(data->pipe);
+					return NULL;
+					pthread_exit(NULL);
+				}
+
+			}
 		}
-		close(fd);
 	}
 }
 
@@ -88,6 +117,40 @@ char *get_file_name(char *path) {
  */
 void copy_file(char *src, char *dest) {
 
+	//open files and check for errors
+	FILE* srcFile = fopen(src, "rb");
+	FILE* destFile;
+
+	if (!srcFile) {
+		perror("error while opening files...");
+		return;
+	} else {
+		destFile = fopen(dest, "wb");
+		 if (!destFile) {
+			 perror("can't write to dest...");
+			 return;
+		 }
+	}
+
+	// memory allocation for the buffer
+	unsigned char* buffer = (unsigned char*) malloc(COPY_BUFFER_SIZE);
+
+	if (!buffer) {
+		perror("error while allocating memory for buffer");
+		return;
+	}
+
+	// read and write the file
+	while (!feof(srcFile)) {
+		fread(buffer, COPY_BUFFER_SIZE, 1, srcFile);
+		fwrite(buffer, COPY_BUFFER_SIZE, 1, destFile);
+	}
+
+
+	// closing the files and release memory
+	fclose(srcFile);
+	fclose(destFile);
+	free(buffer);
 }
 
 /*
@@ -100,9 +163,31 @@ void copy_file(char *src, char *dest) {
  */
 void *run_copier(void *param) {
 
+	// gets the thread data
+
+	CopierData* copierData = (CopierData*) param;
+	BoundedBuffer* buff = (BoundedBuffer*) copierData->buff;
+	char* src;
+
+	// checks if the program wants to exit
+	while ( (src = bounded_buffer_dequeue(buff)) != NULL) {
+		//TODO check for error while malloc
+		char* name = get_file_name(src);
+		char* dest = (char*) malloc(sizeof(char) * strlen(copierData->dest) + strlen(name));
+		strcpy(dest, copierData->dest);
+		strcat(dest, name);
+
+		copy_file(src, dest);
+
+		free(dest);
+	}
+
+	pthread_exit(NULL);
 }
 
-
+/**
+ * Helper function for printing a queue
+ */
 void printBuffer(BoundedBuffer* b) {
 
 	if (b->size > 0) {
@@ -130,34 +215,36 @@ void printBuffer(BoundedBuffer* b) {
  */
 int main(int argc, char *argv[]) {
 
-	BoundedBuffer boundedBuffer;
-	BoundedBuffer* p_boundedBuffer = &boundedBuffer;
+	//TODO add command line arguments
+
+	pthread_t listener, copier;
+
+	// creates the buffer
 	int capacity = 2;
 
+	// init data for the threads
+	BoundedBuffer buff;
+	BoundedBuffer* p_boundedBuffer = &buff;
 	bounded_buffer_init(p_boundedBuffer, capacity);
 
-	char* s1 = "hello";
-	char* s2 = "world";
-	char* s3 = "bye";
+	ListenerData listenerDate;
+	listenerDate.buff = p_boundedBuffer;
+	listenerDate.pipe = "./the_pipe";
 
+	CopierData copierData;
+	copierData.buff = p_boundedBuffer;
+	copierData.dest = "./copies/";
 
-	bounded_buffer_enqueue(p_boundedBuffer, s1);
-	bounded_buffer_enqueue(p_boundedBuffer, s2);
+	// runs the threads
+	pthread_create(&listener, NULL, run_listener, (void*)(&listenerDate));
+	pthread_create(&copier, NULL, run_copier, (void*)(&copierData));
 
-	printBuffer(p_boundedBuffer);
+	// Join threads
+	pthread_join(listener, NULL);
+	pthread_join(copier, NULL);
 
-	printf("\n\n");
-
-	bounded_buffer_dequeue(p_boundedBuffer);
-
-	bounded_buffer_enqueue(p_boundedBuffer, s3);
-
-	printBuffer(p_boundedBuffer);
-
-	printf("\n\n");
-
-
-
+	// Free allocated resources
+	bounded_buffer_destroy(p_boundedBuffer);
 
 	return 0;
 }
